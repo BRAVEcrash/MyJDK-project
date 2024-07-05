252170584cc6447d2b80af44fe584671c2bc7356
BRAVE <102480883+BRAVEcrash@users.noreply.github.com>
2024-07-05 18:46:17 +0900

    JDK

---
+++ myJDK.java
@@ -0,0 +1,13 @@
+public class myJDK {
+    public static void main(String[] args) {
+
+        //comment
+        System.out.println("Hello World!");
+        System.out.println("Welcome to Java!\n!!!");
+
+        /* my
+        big
+        comment
+         */
+    }
+}
